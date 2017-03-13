ccc6b7460b2cac9347577bfc5aa6bbb78048d5c8
amdPC <625580322@qq.com>
2017-03-13 10:48:42 +0800

    start

---
+++ Assets/camera.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour {
+
+    private Transform m_Transform;
+	// Use this for initialization
+	void Start () {
+        m_Transform = gameObject.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //m_Transform.transform.rotation.z = 0;
+        Quaternion a = gameObject.transform.rotation;
+        //Unity不允许直接设置Rotation中X.Y.Z的值，我们需要这样做才可以：
+
+        //先来一个Quaternion :
+        Quaternion quate = Quaternion.identity;
+
+        //然后设置他的角度:
+        quate.eulerAngles = new Vector3(a.x, a.y, 0); // 表示设置x轴方向旋转了45度
+
+        //最后再把quate付给你要操作的Gameobject：
+        m_Transform.transform.rotation = quate;
+    }
+}
---
+++ Assets/player.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+    private Transform m_Transform;
+    private Transform came;
+
+	// Use this for initialization
+	void Start () {
+         m_Transform = gameObject.GetComponent<Transform>();
+         came = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float speed = 0.01f;
+
+        if (Input.GetKey (KeyCode.W))
+        {
+            m_Transform.Translate(-1 * speed,0,0);
+            //m_Transform.Translate(Vector3.forward * speed, Camera.main.transform);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            m_Transform.Translate(0,0,-1 * speed);
+            //m_Transform.Translate(Vector3.left * speed, Camera.main.transform);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            m_Transform.Translate(0, 0, 1 * speed);
+            //m_Transform.Translate(Vector3.right * speed, Camera.main.transform);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            m_Transform.Translate(1 * speed, 0, 0);
+            //m_Transform.Translate(Vector3.back * speed, Camera.main.transform);
+        }
+	}
+}
